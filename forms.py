b204326fdc6bd75345454318cdc2174306a2f12b
IDGS80120001481 <20001481@alumnos.utleon.edu.mx>
2024-02-21 09:19:05 -0600

    CMGJ 21-02-2024

--- forms.py
+++ forms.py
@@ -1,27 +1,23 @@
-from wtforms import Form
-from wtforms import StringField, TelField, IntegerField
-
-from wtforms import validators
-from wtforms.validators import DataRequired, Email
-
-
+from wtforms import Form, StringField, IntegerField
+from wtforms.validators import DataRequired, Length, Email
 
 class UserForm(Form):
-    nombre = StringField('nombre', [
-    validators.DataRequired(message="El campo es requerido"),
-    validators.Length(min=4, max=10, message="Ingresa un nombre válido")
+    nombre = StringField('Nombre', validators=[
+        DataRequired(message="El nombre es requerido"),
+        Length(min=4, max=50, message="Ingresa un nombre válido (entre 4 y 50 caracteres)")
     ])
-    email = StringField("email", [
-    validators.DataRequired(message="El campo es requerido"),
-    validators.Length(min=4, max=10, message="Ingresa un nombre válido")
+    email = StringField("Email", validators=[
+        DataRequired(message="El email es requerido"),
+        Email(message="Ingresa un email válido")
     ])
-    apaterno = TelField("apaterno",[
-    validators.DataRequired(message="El campo es requerido"),
-    validators.Length(min=4, max=10, message="Ingresa un nombre válido")
+    apaterno = StringField("Apellido Paterno", validators=[
+        DataRequired(message="El apellido paterno es requerido"),
+        Length(min=4, max=50, message="Ingresa un apellido paterno válido (entre 4 y 50 caracteres)")
     ])
-    amaterno = StringField('amaterno', [
-    validators.DataRequired(message="El campo es requerido"),
-    validators.Length(min=4, max=10, message="Ingresa un nombre válido")
+    amaterno = StringField('Apellido Materno', validators=[
+        DataRequired(message="El apellido materno es requerido"),
+        Length(min=4, max=50, message="Ingresa un apellido materno válido (entre 4 y 50 caracteres)")
     ])
-    edad = IntegerField('edad', [
-    validators.Email(message='Ingrese una edad válida')])
+    edad = IntegerField('Edad', validators=[
+        DataRequired(message="La edad es requerida")
+    ])
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
 import forms
 from wtforms import validators
+from flask import flash
+from flask import g
+
+
+
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+app.secret_key = "Esta madre se puso buena"
+
+@app.before_request
+def before():
+    g.nombre = 'Mario' 
+    print('before 1')
+
+@app.after_request
+def after(response):
+    print('after 3')
+    return response
 
 @app.route("/")
 def index():
@@ -10,12 +32,14 @@ def index():
 
 @app.route("/alumnos", methods=['GET','POST'])
 def alumnos():
+    print("alumno {}".format(g.nombre))
     nom = ''
     apa = ''
     ama = ''
+    edad = ''
 
     alumno_clase = forms.UserForm(request.form)
-    if request.method == 'POST' and alumno_clase.validate():
+    if request.method == 'POST':
         nom = alumno_clase.nombre.data 
         apa = alumno_clase.apaterno.data 
         ama = alumno_clase.amaterno.data 
@@ -23,6 +47,9 @@ def alumnos():
         print('Nombre: {}'.format(nom))
         print('Apaterno: {}'.format(apa))
         print('Amaterno: {}'.format(ama))
+
+        mensaje = 'Bienvenido {}'.format(nom)
+        flash(mensaje)
     return render_template("alumnos2.html", form=alumno_clase, nom = nom, apa = apa, ama = ama)
 
 @app.route("/maestros")
